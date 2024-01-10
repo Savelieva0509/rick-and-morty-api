46534219f5b7bcf948656afd2a6ff750dcddb8b7
Babenko Liudmyla <savelieva0509@gmail.com>
2024-01-10 13:36:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/rickandmortyapi">
+    <BrowserRouter basename="/rick-and-morty-api">
       <App />
     </BrowserRouter>
   </React.StrictMode>
